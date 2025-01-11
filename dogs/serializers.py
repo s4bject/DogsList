28cfb851684e0fa1f5093bdc6c471cd2e7a52494
s4bject <artyon20026@gmail.com>
2025-01-11 18:05:15 +0700

    Add dogs&breeds count and requirements.txt

--- dogs/serializers.py
+++ dogs/serializers.py
@@ -1,20 +1,30 @@
-from rest_framework.serializers import ModelSerializer, ValidationError
-
+from rest_framework.fields import SerializerMethodField
+from rest_framework.serializers import ModelSerializer
 from dogs.models import Dog, Breed
+from django.db.models import Avg, Count
 
 
 class DogSerializer(ModelSerializer):
+    breed_count = SerializerMethodField()
+
     class Meta:
         model = Dog
         fields = '__all__'
 
+    def get_breed_count(self, obj):
+        return getattr(obj, 'breed_count', 0)
+
 
 class BreedSerializer(ModelSerializer):
+    dog_count = SerializerMethodField()
+    average_age = SerializerMethodField()
+
     class Meta:
         model = Breed
         fields = '__all__'
 
-    def validate_data(self, value):
-        if value < 1 or value > 5:
-            raise ValidationError("Некорректное значение")
-        return value
+    def get_dog_count(self, obj):
+        return getattr(obj, 'dog_count', 0)
+
+    def get_average_age(self, obj):
+        return getattr(obj, 'average_age', None)
--- dogs/views.py
+++ dogs/views.py
@@ -1,5 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
-
+from django.db.models import Avg, Count
 from dogs.models import Dog, Breed
 from dogs.serializers import DogSerializer, BreedSerializer
 
@@ -8,6 +8,18 @@ class DogViewSet(ModelViewSet):
     queryset = Dog.objects.all()
     serializer_class = DogSerializer
 
+    def get_queryset(self):
+        return Dog.objects.annotate(
+            breed_count=Count('breed__dog')
+        )
+
+
 class BreedViewSet(ModelViewSet):
     queryset = Breed.objects.all()
     serializer_class = BreedSerializer
+
+    def get_queryset(self):
+        return Breed.objects.annotate(
+            dog_count=Count('dog'),
+            average_age=Avg('dog__age')
+        )
