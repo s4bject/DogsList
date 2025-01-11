fb05e2e752862ced1a82bb6e304c81e285f1fbff
s4bject <artyon20026@gmail.com>
2025-01-11 23:36:22 +0700

    Fixed errors in logic

--- dogs_django/dogs/serializers.py
+++ dogs_django/dogs/serializers.py
@@ -1,27 +1,17 @@
+from django.db.models import Avg
 from rest_framework.fields import SerializerMethodField
 from rest_framework.serializers import ModelSerializer
 from dogs.models import Dog, Breed
 
 
-class DogSerializer(ModelSerializer):
-    """Сериализатор для модели собаки."""
-    breed_count = SerializerMethodField()
+class DogListSerializer(ModelSerializer):
+    """Сериализатор для списка собак."""
     breed_name = SerializerMethodField()
+    breed_avg_age = SerializerMethodField()
 
     class Meta:
         model = Dog
         fields = '__all__'
-
-    def get_breed_count(self, obj):
-        """Получает количество собак в породе.
-
-        Args:
-            obj (Dog): Экземпляр модели собаки.
-
-        Returns:
-            int: Количество собак в породе.
-        """
-        return getattr(obj, 'breed_count', 0)
 
     def get_breed_name(self, obj):
         """Получает название породы собаки.
@@ -34,11 +24,53 @@ class DogSerializer(ModelSerializer):
         """
         return obj.breed.name if obj.breed else None
 
+    def get_breed_avg_age(self, obj):
+        """Получает средний возраст собак в породе из аннотации для списка.
+
+        Args:
+            obj (Dog): Экземпляр модели собаки.
+
+        Returns:
+            float: Средний возраст собак в породе или None, если информация отсутствует.
+        """
+        return getattr(obj, 'breed_avg_age', None)
+
+class DogDetailSerializer(ModelSerializer):
+    """Сериализатор для детального запроса собаки."""
+    breed_name = SerializerMethodField()
+    breed_count = SerializerMethodField()
+
+    class Meta:
+        model = Dog
+        fields = '__all__'
+
+    def get_breed_name(self, obj):
+        """Получает название породы собаки.
+
+        Args:
+            obj (Dog): Экземпляр модели собаки.
+
+        Returns:
+            str: Название породы собаки или None, если порода не указана.
+        """
+        return obj.breed.name if obj.breed else None
+
+    def get_breed_count(self, obj):
+        """Получает количество собак той же породы из аннотации для детального запроса.
+
+        Args:
+            obj (Dog): Экземпляр модели собаки.
+
+        Returns:
+            int: Количество собак той же породы или 0, если информация отсутствует.
+        """
+        return getattr(obj, 'breed_count', 0)
+
+
 
 class BreedSerializer(ModelSerializer):
     """Сериализатор для модели породы."""
     dog_count = SerializerMethodField()
-    average_age = SerializerMethodField()
 
     class Meta:
         model = Breed
@@ -54,14 +86,3 @@ class BreedSerializer(ModelSerializer):
             int: Количество собак в породе.
         """
         return getattr(obj, 'dog_count', 0)
-
-    def get_average_age(self, obj):
-        """Получает средний возраст собак в породе.
-
-        Args:
-            obj (Breed): Экземпляр модели породы.
-
-        Returns:
-            float: Средний возраст собак в породе или None, если информация отсутствует.
-        """
-        return getattr(obj, 'average_age', None)
--- dogs_django/dogs/views.py
+++ dogs_django/dogs/views.py
@@ -1,24 +1,30 @@
 from rest_framework.viewsets import ModelViewSet
 from django.db.models import Avg, Count
 from dogs.models import Dog, Breed
-from dogs.serializers import DogSerializer, BreedSerializer
+from dogs.serializers import BreedSerializer, DogDetailSerializer, DogListSerializer
 
 
 class DogViewSet(ModelViewSet):
     """ViewSet для модели собаки."""
     queryset = Dog.objects.all()
-    serializer_class = DogSerializer
+
+    def get_serializer_class(self):
+        """Определяет, какой сериализатор использовать в зависимости от действия."""
+        if self.action == 'retrieve':
+            return DogDetailSerializer
+        return DogListSerializer
 
     def get_queryset(self):
-        """Получает queryset с дополнительной аннотацией для подсчета количества собак в породах.
+        """Получает queryset с аннотациями для списка и детального запроса.
 
         Returns:
-            QuerySet: Отфильтрованный queryset с добавленным полем breed_count.
+            QuerySet: Отфильтрованный queryset с добавленными полями breed_avg_age или breed_count.
         """
-        return Dog.objects.annotate(
-            breed_count=Count('breed__dog')
-        )
-
+        if self.action == 'list':
+            return Dog.objects.annotate(breed_avg_age=Avg('breed__dog__age'))
+        elif self.action == 'retrieve':
+            return Dog.objects.annotate(breed_count=Count('breed__dog'))
+        return super().get_queryset()
 
 class BreedViewSet(ModelViewSet):
     """ViewSet для модели породы."""
@@ -34,5 +40,4 @@ class BreedViewSet(ModelViewSet):
         """
         return Breed.objects.annotate(
             dog_count=Count('dog'),
-            average_age=Avg('dog__age')
         )
